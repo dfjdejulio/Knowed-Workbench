08766d001771c346b9c73b4d5831a97b7efe8604
Doug DeJulio <ddj@aisb.org>
2014-04-17 00:03:08 -0400

    The basic controller for this app.

---
+++ Knowed Workbench/KWController.h
@@ -0,0 +1,13 @@
+//
+//  KWController.h
+//  Knowed Workbench
+//
+//  Created by Doug DeJulio on 4/16/14.
+//  Copyright (c) 2014 AISB. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KWController : NSObject
+
+@end
---
+++ Knowed Workbench/KWController.m
@@ -0,0 +1,13 @@
+//
+//  KWController.m
+//  Knowed Workbench
+//
+//  Created by Doug DeJulio on 4/16/14.
+//  Copyright (c) 2014 AISB. All rights reserved.
+//
+
+#import "KWController.h"
+
+@implementation KWController
+
+@end
